5884ad75e15abd44aa74cc00f2ba36ba325e91ea
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2022-02-22 22:49:02 +0000

    Test interaction OK

--- Assets/Scripts/Interactions/OpenDoor.cs
+++ Assets/Scripts/Interactions/OpenDoor.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     Animator animator;
+    bool isDoorOpen;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        isDoorOpen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Activate()
+    public void InteractWithDoor()
     {
-        animator.SetTrigger("Activate");
+        if (isDoorOpen) {
+            Debug.Log("Close the door");
+            animator.SetTrigger("Desactivate");
+        }
+        else {
+            Debug.Log("Open the door");
+            animator.SetTrigger("Activate");
+        }
+
+        isDoorOpen = !isDoorOpen;
     }
 }

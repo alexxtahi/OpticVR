940e53dc4c7c3931f79c470fc580d5467b368c32
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2022-02-25 10:17:34 +0000

    Adding CameraView

---
+++ Assets/Scripts/Interactions/PhoneCamera.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OpticVR
+{
+    public class PhoneCamera : MonoBehaviour
+    {
+        private bool camAvailable;
+        private WebCamTexture frontCam;
+        private Texture defaultBackground;
+
+        public RawImage background;
+        public AspectRatioFitter fit;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                defaultBackground = background.texture;
+                WebCamDevice[] devices = WebCamTexture.devices;
+                // If no camera available
+                if (devices.Length == 0)
+                {
+                    Debug.Log("No camera detected");
+                    camAvailable = false;
+                    return;
+                }
+                // Try to get the front camera in devices list
+                for (int i = 0; i < devices.Length; i++)
+                {
+                    frontCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                }
+                // If no camera has detected
+                if (frontCam == null)
+                {
+                    Debug.Log("Unable to find the front camera");
+                    return;
+                }
+                // Display camera preview
+                frontCam.Play();
+                background.texture = frontCam;
+                camAvailable = true;
+            } else
+            {
+                Debug.Log("This device is not an Android");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!camAvailable) return;
+
+            float ratio = (float)frontCam.width / (float)frontCam.height;
+            fit.aspectRatio = ratio;
+
+            float scaleY = frontCam.videoVerticallyMirrored ? -1f : 1f;
+            background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+            int orient = -frontCam.videoRotationAngle;
+            background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        }
+    }
+}
